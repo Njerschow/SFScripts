cf87851ac8c862ffa649e706c0910d3f85dc1ae9
Njerschow <nj18@nyu.edu>
2018-05-02 11:49:38 -0400

    starting query work

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ function readAVM() {
 	rows = str.split(/\r?\n|\r/);
 	rows.forEach( (row)=> {
 		cols = row.split(/,/);
-		QueryTable[cols[0]] = cols[2];
+		QueryTable[cols[0]] = cols[1];
 	});
 }
 
@@ -125,14 +125,112 @@ function sendQuery(d) {
 }
 
 function resolveQuery(phrase, X) {
-	
+	let date  new Date(Date.now());
+	const year = date.getUTCFullYear();
+	const month = date.getUTCMonth()+1;
+	const quarter = Math.ceil(month/3);
+
+	let Query = "";
+
+	let action = QueryTable[phrase];
+
+	//Code to get action taken --> action
+	switch (action) {
+		case "Get first quarter sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and FIscalQuarter=1 and Account.name LIKE '"+X+"'";
+		break;
+		case "Get second quarter sales numberr":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and FIscalQuarter=2 and Account.name LIKE '"+X+"'";
+		break;
+		case "Get third quarter sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and FIscalQuarter=3 and Account.name LIKE '"+X+"'";
+		break;
+		case "Get fourth quarter sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and FIscalQuarter=4 and Account.name LIKE '"+X+"'";
+		break;
+		case "Get last quarter sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and FIscalQuarter="+(quarter-1)+" and Account.name LIKE '"+X+"'";
+		break;
+		case "Get current quarter sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and FIscalQuarter="+quarter+" and Account.name LIKE '"+X+"'";
+		break;
+		case "Get last year sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+(year-1)+" and Account.name LIKE '"+X+"'";
+		break;
+		case "Get current year sales number":
+		Query = "SELECT Amount, Account.name FROM Opportunity WHERE FiscalYear="+year+" and Account.name LIKE '"+X+"'";
+		break;
+		case "Get last sale amount for client":
+		break;
+		case "Get last sale UNITS amount for client":
+		break;
+		case "Pull last call AND meeting date with X":
+		break;
+		case "Pull last meeting date with X":
+		break;
+		case "Pull last call date with X":
+		break;
+		case "Pull the sales or customer support form called X":
+		break;
+		case "Pull the status for the Support Ticket #":
+		break;
+		case "Pull the status for open / latest Support Ticket PLUS Email":
+		break;
+		case "Pull the status for the Support Ticket":
+		break;
+		case "Pull status for open / latest Support Ticket":
+		break;
+		case "Pull Stage / Status and Next Steps":
+		break;
+		case "Pull Next Steps":
+		break;
+		case "Pull activity date":
+		break;
+		case "Get close date of last sale":
+		break;
+		case "Get main contact person at client ":
+		break;
+		case "Get main sales contact for client ":
+		break;
+		case "Get ALL sales contacts for client ":
+		break;
+		case "Get product purchased":
+		break;
+		case "Get # of seats sold in last sale":
+		break;
+		case "Get # of units sold in last sale":
+		break;
+		case "Get date proposal was sent":
+		break;
+		case "Get close probability":
+		break;
+		case "Get pricing for X":
+		break;
+		case "Get pricing on last sale for client":
+		break;
+		case "Get contact info":
+		break;
+		case "Get renewal date":
+		break;
+		case "Get notes":
+		break;
+		case "Get employee number for X":
+		break;
+		case "Get annual revenue for X":
+		break;
+		case "Get deal value ":
+		break;
+		case "Get support contact":
+		break;
+
+	}
 
 }
 
 
 function main() {
 	stdin.addListener("data", sendQuery);
-
+	readAVM();
 
 
 }
